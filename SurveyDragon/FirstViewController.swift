3e6dc96bd9dfdf5a4cc4947ef295224d6533c332
Philip Nguyen <philipnguyen@philips-air>
2018-10-21 17:55:01 -0700

    Initial Commit

---
+++ SurveyDragon/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SurveyDragon
+//
+//  Created by Philip Nguyen on 10/21/18.
+//  Copyright © 2018 Philip Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SurveyDragon/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SurveyDragon
+//
+//  Created by Philip Nguyen on 10/21/18.
+//  Copyright © 2018 Philip Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
