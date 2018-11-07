76495996b03e908bf263603b63394d502fa06552
Philip Nguyen <philipnguyen@Philips-MacBook-Air.local>
2018-11-07 11:41:24 -0800

    storyboard almost support long answer question survey creation

---
+++ SurveyDragon/ComposeSurveyViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ComposeSurveyViewController.swift
+//  SurveyDragon
+//
+//  Created by Philip Nguyen on 11/5/18.
+//  Copyright © 2018 Philip Nguyen. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+import FirebaseAuth
+
+class ComposeSurveyViewController: UIViewController {
+    
+    
+    
+    let userID = Auth.auth().currentUser?.uid
+    
+    var ref: DatabaseReference?
+    
+    @IBOutlet weak var surveyNameView: UITextView! {
+    
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ref = Database.database().reference()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func createSurvey(_sender: Any) {
+        ref?.child("surveys").childByAutoId().setValue(["title": surveyNameView,
+                                                        "author": userID!])
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func appendQuestion(_sender: Any) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SurveyDragon/Survey.swift
@@ -0,0 +1,14 @@
+//
+//  Survey.swift
+//  SurveyDragon
+//
+//  Created by Philip Nguyen on 11/6/18.
+//  Copyright © 2018 Philip Nguyen. All rights reserved.
+//
+
+import Foundation
+
+class Survey {
+    var Title: String
+    var 
+}
