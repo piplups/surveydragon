1a83ecbc9cc7eec3763a22c369642dbce17085a4
Khoa <khoavo910@gmail.com>
2018-11-26 19:45:40 -0800

    My survey works

--- SurveyDragon/MySurveyResultsViewController.swift
+++ SurveyDragon/MySurveyResultsViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class MySurveyResultsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var surveys = [String]()
+    var surveys = ["How is your semester going?", "Are you stressed?", "Excited for winter break?",
+                   "Computer Science?", "milk or cereal first?", "what time you wake up?",
+                   "do you like mac n cheese", "this project needs to work"]
     
+    var key: String?
+    var userID = Auth.auth().currentUser?.uid
+    
+    var ref: DatabaseReference!
+    
+    
+    @IBOutlet weak var mySurveyTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        ref = Database.database().reference()
+
+        mySurveyTableView.dataSource = self
+        mySurveyTableView.delegate = self
+        
         // TODO: make function to pull from database
         
         // Do any additional setup after loading the view.
+        self.loadFromFireBase()
+
+        self.mySurveyTableView.reloadData()
+
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,6 +49,27 @@ class MySurveyResultsViewController: UIViewController, UITableViewDataSource, UI
         
         // TODO:
         cell.textLabel?.text = surveys[indexPath.row]
+        return cell
+
+    }
+    
+    func loadFromFireBase(){
+        
+        ref.child("Authors/\(userID!)").observe(.value, with: { (snapshot) in
+
+            self.surveys.removeAll()
+            for user_child in (snapshot.children) {
+                let user_snap = user_child as! DataSnapshot
+                let dict = user_snap.value as! [String: String?]
+                var question = dict["title"] as? String
+                self.surveys.append(question!)
+                self.mySurveyTableView.reloadData()
+            }
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
     }
     
     /*
